0d56514ed54959cae7a861a0c007680c90a4bbf4
RafifKI <106825044+roboloc@users.noreply.github.com>
2025-07-10 21:38:42 +0700

    feat:add login (#2)

---
+++ src/auth.ts
@@ -0,0 +1,35 @@
+import NextAuth from "next-auth";
+import Creadentials from "next-auth/providers/credentials";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+    //tambahin import google credential ataupun lainnya di sini
+    Creadentials({
+      async authorize(user) {
+        if (user) return user;
+        return null;
+      },
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+    maxAge: 2 * 60 * 60,
+  },
+  pages: {
+    signIn: "/login",
+  },
+  callbacks: {
+    async signIn() {
+      //dan tambahkan di signIn
+      return true;
+    },
+    async jwt({ token, user }) {
+      if (user) token.user = user;
+      return token;
+    },
+    async session({ session, token }: any) {
+      if (token.user) session.user = token.user;
+      return session;
+    },
+  },
+});
